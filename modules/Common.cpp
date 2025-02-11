839578422da4536905699ad3fa741eb30824c8de
rainmakerv2 <30595646+jpau02@users.noreply.github.com>
2025-02-11 22:04:02 +0800

    6.3

--- modules/Common.cpp
+++ modules/Common.cpp
@@ -27,7 +27,7 @@ std::filesystem::path installPath;
 std::filesystem::path installUpdatePath;
 std::filesystem::path SaveDir;
 std::filesystem::path shadPs4Executable;
-char VERSION[] = "Release6.2";
+char VERSION[] = "Release6.3";
 
 std::filesystem::path GetShadUserDir() {
     auto user_dir = std::filesystem::current_path() / "user";
