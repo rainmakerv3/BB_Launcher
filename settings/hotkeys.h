e305cd95de1168e4a79fcf92c15f1b471cebda77
rainmakerv3 <30595646+rainmakerv3@users.noreply.github.com>
2025-08-27 16:07:13 +0800

    WIP

---
+++ settings/hotkeys.h
@@ -0,0 +1,60 @@
+// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <filesystem>
+#include <QDialog>
+#include <QFuture>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_gamepad.h>
+
+namespace Ui {
+class hotkeys;
+}
+
+class hotkeys : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit hotkeys(QWidget* parent = nullptr);
+    ~hotkeys();
+
+signals:
+    void PushGamepadEvent();
+
+private:
+    bool eventFilter(QObject* obj, QEvent* event) override;
+    void CheckMapping(QPushButton*& button);
+    void StartTimer(QPushButton*& button, bool isButton);
+    void DisableMappingButtons();
+    void EnableMappingButtons();
+    void SaveHotkeys(bool CloseOnSave);
+    void LoadHotkeys();
+    void pollSDLEvents();
+    void CheckGamePad();
+    void SetMapping(QString input);
+    void Cleanup();
+    void createHotkeyFile(std::filesystem::path hotkey_file);
+
+    bool EnableButtonMapping = false;
+    bool MappingCompleted = false;
+    bool L2Pressed = false;
+    bool R2Pressed = false;
+    int MappingTimer;
+    QString mapping;
+    QTimer* timer;
+    QPushButton* MappingButton;
+    SDL_Gamepad* h_gamepad = nullptr;
+    SDL_JoystickID* h_gamepads;
+
+    // use QMap instead of QSet to maintain order of inserted strings
+    QMap<int, QString> pressedButtons;
+    QList<QPushButton*> ButtonsList;
+    QFuture<void> Polling;
+
+    Ui::hotkeys* ui;
+
+protected:
+    void closeEvent(QCloseEvent* event) override {
+        Cleanup();
+    }
+};
