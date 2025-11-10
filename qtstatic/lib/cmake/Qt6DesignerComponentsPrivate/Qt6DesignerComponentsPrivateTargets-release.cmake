cc7972a04c302c7cb3a5dd36bcd6d411601c4d88
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2025-11-10 15:05:11 +0800

    Build with static libs to eliminate all DLLs

---
+++ qtstatic/lib/cmake/Qt6DesignerComponentsPrivate/Qt6DesignerComponentsPrivateTargets-release.cmake
@@ -0,0 +1,59 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Qt6::DesignerComponentsPrivate" for configuration "Release"
+set_property(TARGET Qt6::DesignerComponentsPrivate APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Qt6::DesignerComponentsPrivate PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/Qt6DesignerComponents.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Qt6::DesignerComponentsPrivate )
+list(APPEND _cmake_import_check_files_for_Qt6::DesignerComponentsPrivate "${_IMPORT_PREFIX}/lib/Qt6DesignerComponents.lib" )
+
+# Import target "Qt6::DesignerComponentsPrivate_resources_1" for configuration "Release"
+set_property(TARGET Qt6::DesignerComponentsPrivate_resources_1 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Qt6::DesignerComponentsPrivate_resources_1 PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_RELEASE ""
+  IMPORTED_OBJECTS_RELEASE "${_IMPORT_PREFIX}/lib/objects-Release/DesignerComponentsPrivate_resources_1/.qt/rcc/qrc_propertyeditor_init.cpp.obj"
+  )
+
+list(APPEND _cmake_import_check_targets Qt6::DesignerComponentsPrivate_resources_1 )
+list(APPEND _cmake_import_check_files_for_Qt6::DesignerComponentsPrivate_resources_1 "${_IMPORT_PREFIX}/lib/objects-Release/DesignerComponentsPrivate_resources_1/.qt/rcc/qrc_propertyeditor_init.cpp.obj" )
+
+# Import target "Qt6::DesignerComponentsPrivate_resources_2" for configuration "Release"
+set_property(TARGET Qt6::DesignerComponentsPrivate_resources_2 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Qt6::DesignerComponentsPrivate_resources_2 PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_RELEASE ""
+  IMPORTED_OBJECTS_RELEASE "${_IMPORT_PREFIX}/lib/objects-Release/DesignerComponentsPrivate_resources_2/.qt/rcc/qrc_formeditor_init.cpp.obj"
+  )
+
+list(APPEND _cmake_import_check_targets Qt6::DesignerComponentsPrivate_resources_2 )
+list(APPEND _cmake_import_check_files_for_Qt6::DesignerComponentsPrivate_resources_2 "${_IMPORT_PREFIX}/lib/objects-Release/DesignerComponentsPrivate_resources_2/.qt/rcc/qrc_formeditor_init.cpp.obj" )
+
+# Import target "Qt6::DesignerComponentsPrivate_resources_3" for configuration "Release"
+set_property(TARGET Qt6::DesignerComponentsPrivate_resources_3 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Qt6::DesignerComponentsPrivate_resources_3 PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_RELEASE ""
+  IMPORTED_OBJECTS_RELEASE "${_IMPORT_PREFIX}/lib/objects-Release/DesignerComponentsPrivate_resources_3/.qt/rcc/qrc_formeditor1_init.cpp.obj"
+  )
+
+list(APPEND _cmake_import_check_targets Qt6::DesignerComponentsPrivate_resources_3 )
+list(APPEND _cmake_import_check_files_for_Qt6::DesignerComponentsPrivate_resources_3 "${_IMPORT_PREFIX}/lib/objects-Release/DesignerComponentsPrivate_resources_3/.qt/rcc/qrc_formeditor1_init.cpp.obj" )
+
+# Import target "Qt6::DesignerComponentsPrivate_resources_4" for configuration "Release"
+set_property(TARGET Qt6::DesignerComponentsPrivate_resources_4 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Qt6::DesignerComponentsPrivate_resources_4 PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_RELEASE ""
+  IMPORTED_OBJECTS_RELEASE "${_IMPORT_PREFIX}/lib/objects-Release/DesignerComponentsPrivate_resources_4/.qt/rcc/qrc_widgetbox_init.cpp.obj"
+  )
+
+list(APPEND _cmake_import_check_targets Qt6::DesignerComponentsPrivate_resources_4 )
+list(APPEND _cmake_import_check_files_for_Qt6::DesignerComponentsPrivate_resources_4 "${_IMPORT_PREFIX}/lib/objects-Release/DesignerComponentsPrivate_resources_4/.qt/rcc/qrc_widgetbox_init.cpp.obj" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
---
+++ qtstatic/mkspecs/modules/qt_ext_harfbuzz.pri
@@ -0,0 +1,3 @@
+QMAKE_INCDIR_HARFBUZZ = D:/Github/qt-6.10.0/qt-everywhere-src-6.10.0/qtbase/src/3rdparty/harfbuzz-ng/include/harfbuzz
+QMAKE_DEFINES_HARFBUZZ = 
+QMAKE_LIBS_HARFBUZZ = $$[QT_INSTALL_LIBS/get]/Qt6BundledHarfbuzz.lib
