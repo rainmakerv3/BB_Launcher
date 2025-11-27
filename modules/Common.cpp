0f3dd0da3edb3b4cb420e8e23ed1791f6f8d9acf
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2025-11-27 22:25:22 +0800

    Start 12.06

--- modules/Common.cpp
+++ modules/Common.cpp
@@ -33,7 +33,7 @@ std::filesystem::path installPath;
 std::filesystem::path installUpdatePath;
 std::filesystem::path shadPs4Executable;
 
-const char VERSION[] = "Release12.05";
+const char VERSION[] = "Release12.06";
 
 std::filesystem::path GetCurrentPath(bool getLinuxFileName) {
     std::filesystem::path currentPath;
