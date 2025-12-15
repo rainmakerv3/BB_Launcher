10c4e23abe89d71dd9306e446aa49d2b3be8b794
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2025-12-15 19:55:26 +0800

    set as 13.00 instead of 12.10

--- modules/Common.cpp
+++ modules/Common.cpp
@@ -33,7 +33,7 @@ std::filesystem::path installPath;
 std::filesystem::path installUpdatePath;
 std::filesystem::path shadPs4Executable;
 
-const char VERSION[] = "Release12.10";
+const char VERSION[] = "Release13.00";
 
 std::filesystem::path GetCurrentPath(bool getLinuxFileName) {
     std::filesystem::path currentPath;
