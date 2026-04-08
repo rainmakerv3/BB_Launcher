ef46ff375914d31308e90e54d61485fd38ef92c2
rainmakerv3 <30595646+rainmakerv3@users.noreply.github.com>
2026-04-08 12:12:52 +0800

    cleanup startup code

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@
 void customMessageHandler(QtMsgType, const QMessageLogContext&, const QString&) {}
 
 int main(int argc, char* argv[]) {
-
 #ifdef Q_OS_MAC
     qputenv("QT_WEBVIEW_PLUGIN", "native");
 #endif
@@ -27,34 +26,9 @@ int main(int argc, char* argv[]) {
 #ifndef USE_WEBENGINE
     QtWebView::initialize();
 #endif
+
     QApplication a(argc, argv);
     QApplication::setStyle("Fusion");
-
-// Check if CPU supports AVX2, show error and quit otherwise
-#if defined(_MSC_VER)
-    std::vector<int> cpuInfo(4);
-    __cpuidex(cpuInfo.data(), 7, 0);
-    const int AVX2_BIT = (1 << 5);
-    bool avx2_available = (cpuInfo[1] & AVX2_BIT) != 0;
-
-    if (!avx2_available) {
-        QMessageBox::information(nullptr, "Unsupported CPU",
-                                 "CPU does not support AVX2 instructions. BBLauncher and ShadPS4 "
-                                 "both require CPU that support AVX2. Quitting application...");
-        return 0;
-    }
-#elif defined(__clang__) || defined(__GNUC__)
-
-#ifndef __APPLE__
-    if (!__builtin_cpu_supports("avx2")) {
-        QMessageBox::information(nullptr, "Unsupported CPU",
-                                 "CPU does not support AVX2 instructions. BBLauncher and ShadPS4 "
-                                 "both require CPU that support AVX2. Quitting application...");
-        return 0;
-    }
-#endif
-
-#endif
 
     QCommandLineParser parser;
     QCommandLineOption noGui("n");
@@ -71,11 +45,11 @@ int main(int argc, char* argv[]) {
     if (!noinstancerunning) {
         QMessageBox::warning(nullptr, "BB_Launcher already running",
                              "Only one instance of BB_Launcher can run at a time");
+        main_window->close();
         return 0;
     }
 
-    if (!noGUIset)
-        main_window->show();
+    noGUIset ? main_window->hide() : main_window->show();
 
     if (!main_window->canLaunch) {
         return 0;
