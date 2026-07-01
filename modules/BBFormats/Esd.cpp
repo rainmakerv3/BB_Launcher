6a1246d15a92e1d25d8d9fb3e0dc7f2eab816e7e
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2026-07-01 16:26:55 +0800

    WIP esd reading is a pain

--- modules/BBFormats/Esd.cpp
+++ modules/BBFormats/Esd.cpp
@@ -73,7 +73,7 @@ bool Esd::ReadEsd(std::vector<char> data) {
 
     int stateCount = 0;
     GetInt32(stateCount);
-    debugLog("stateGroupCount: " + std::to_string(stateCount));
+    debugLog("stateCount: " + std::to_string(stateCount));
 
     GetInt32(intBuffer); // since longformat should be true, should be 56
     debugLog("56 check: " + std::to_string(intBuffer));
@@ -184,17 +184,17 @@ bool Esd::ReadEsd(std::vector<char> data) {
         GetInt64(statesOffset);
         debugLog("statesOffset: " + std::to_string(statesOffset));
 
-        int64_t stateCount;
-        GetInt64(stateCount);
-        debugLog("stateCount: " + std::to_string(stateCount));
+        int64_t stateCountinGroup;
+        GetInt64(stateCountinGroup);
+        debugLog("stateCountinGroup: " + std::to_string(stateCountinGroup));
 
         int64_t check;
         GetInt64(check); // should be equal to statesOffset
         debugLog("should be equal to statesoffset: " + std::to_string(check));
 
-        std::vector<int64_t> offsets(stateCount);
-        for (int i = 0; i < stateCount; i++)
-            offsets[i] = statesOffset + i * stateSize;
+        std::vector<int64_t> offsets(stateCountinGroup);
+        for (int j = 0; j < stateCountinGroup; j++)
+            offsets[j] = statesOffset + j * stateSize;
 
         // Every state group with more than one state has a dummy state after the end
         // that's identical to the first state original assert it, but I won't
@@ -206,6 +206,148 @@ bool Esd::ReadEsd(std::vector<char> data) {
         stateGroupOffsetMap[id] = offsets;
     }
 
+    std::unordered_map<int64_t, State> stateMap(stateCount);
+    for (int i = 0; i < stateCount; i++) {
+        State state;
+        int64_t offset = static_cast<int64_t>(istream->tellg() - dataStart);
+
+        GetInt64(state.id);
+        debugLog("state.id: " + std::to_string(state.id));
+
+        int64_t conditionOffsetsOffset;
+        GetInt64(conditionOffsetsOffset);
+        debugLog("conditionOffsetsOffset: " + std::to_string(conditionOffsetsOffset));
+        int64_t conditionOffsetCount;
+        GetInt64(conditionOffsetCount);
+        debugLog("conditionOffsetCount: " + std::to_string(conditionOffsetCount));
+
+        int64_t entryCommandsOffset;
+        GetInt64(entryCommandsOffset);
+        debugLog("entryCommandsOffset: " + std::to_string(entryCommandsOffset));
+        int64_t entryCommandCount;
+        GetInt64(entryCommandCount);
+        debugLog("entryCommandCount: " + std::to_string(entryCommandCount));
+
+        int64_t exitCommandsOffset;
+        GetInt64(exitCommandsOffset);
+        debugLog("exitCommandsOffset: " + std::to_string(exitCommandsOffset));
+        int64_t exitCommandCount;
+        GetInt64(exitCommandCount);
+        debugLog("exitCommandCount: " + std::to_string(exitCommandCount));
+
+        int64_t whileCommandsOffset;
+        GetInt64(whileCommandsOffset);
+        debugLog("whileCommandsOffset: " + std::to_string(whileCommandsOffset));
+        int64_t whileCommandCount;
+        GetInt64(whileCommandCount);
+        debugLog("whileCommandCount: " + std::to_string(whileCommandCount));
+
+        StepIn(0, *istream);
+        {
+            istream->seekg(dataStart + conditionOffsetsOffset);
+            for (int j = 0; j < conditionOffsetCount; j++) {
+                int64_t off;
+                GetInt64(off);
+                debugLog("conditionOffset: " + std::to_string(j) + +": " + std::to_string(off));
+                state.conditionOffsets.push_back(off);
+            }
+
+            istream->seekg(dataStart + entryCommandsOffset);
+            for (int j = 0; j < entryCommandCount; j++) {
+                AddCommandCall(state.entryCommands, dataStart);
+            }
+
+            istream->seekg(dataStart + exitCommandsOffset);
+            for (int j = 0; j < exitCommandCount; j++) {
+                AddCommandCall(state.exitCommands, dataStart);
+            }
+
+            istream->seekg(dataStart + whileCommandsOffset);
+            for (int j = 0; j < whileCommandCount; j++) {
+                AddCommandCall(state.whileCommands, dataStart);
+            }
+        }
+        StepOut(*istream);
+
+        stateMap[offset] = state;
+        states.push_back(state);
+    }
+
+    std::unordered_map<int64_t, Condition> conditionMap(conditionCount);
+    for (int i = 0; i < conditionCount; i++) {
+        Condition cond;
+        int64_t offset = static_cast<int64_t>(istream->tellg() - dataStart);
+
+        GetInt64(cond.stateOffset);
+        debugLog("cond.stateOffset: " + std::to_string(cond.stateOffset));
+
+        int64_t passCommandsOffset;
+        GetInt64(passCommandsOffset);
+        debugLog("passCommandsOffset: " + std::to_string(passCommandsOffset));
+
+        int64_t passCommandCount;
+        GetInt64(passCommandCount);
+        debugLog("passCommandCount: " + std::to_string(passCommandCount));
+
+        int64_t conditionOffsetsOffset;
+        GetInt64(conditionOffsetsOffset);
+        debugLog("conditionOffsetsOffset: " + std::to_string(conditionOffsetsOffset));
+
+        int64_t conditionOffsetCount;
+        GetInt64(conditionOffsetCount);
+        debugLog("conditionOffsetCount: " + std::to_string(conditionOffsetCount));
+
+        int64_t evaluatorOffset;
+        GetInt64(evaluatorOffset);
+        debugLog("evaluatorOffset: " + std::to_string(evaluatorOffset));
+
+        int64_t evaluatorLength;
+        GetInt64(evaluatorLength);
+        debugLog("evaluatorLength: " + std::to_string(evaluatorLength));
+
+        StepIn(0, *istream);
+        {
+            istream->seekg(dataStart + passCommandsOffset);
+            for (int j = 0; j < passCommandCount; j++) {
+                AddCommandCall(cond.passCommands, dataStart);
+            }
+
+            istream->seekg(dataStart + conditionOffsetsOffset);
+            for (int j = 0; j < conditionOffsetCount; j++) {
+                int64_t off;
+                GetInt64(off);
+                debugLog("conditionoffset: " + std::to_string(off));
+                cond.conditionOffsets.push_back(off);
+            }
+
+            StepIn(0, *istream);
+            GetBytes(cond.evaluator, evaluatorLength);
+            StepOut(*istream);
+        }
+        StepOut(*istream);
+
+        conditionMap[offset] = cond;
+    }
+
+    for (auto& state : states) {
+        // getconditions
+    }
+
+    /* holy crap T_T
+             StateGroups = new Dictionary<long, Dictionary<long, State>>(stateGroupCount);
+             var groupedStateOffsets = new Dictionary<long, Dictionary<long, long>>();
+             foreach (long stateGroupID in stateGroupOffsets.Keys)
+             {
+                 long[] stateOffsets = stateGroupOffsets[stateGroupID];
+                 Dictionary<long, State> stateGroup = TakeStates(stateSize, stateOffsets, states,
+     out Dictionary<long, long> stateIDs); StateGroups[stateGroupID] = stateGroup;
+                 groupedStateOffsets[stateGroupID] = stateIDs;
+
+                  foreach (State state in stateGroup.Values)
+                      foreach (Condition condition in state.Conditions)
+                          condition.GetStateAndConditions(stateIDs, conditions);
+              }
+    */
     istream.reset();
     return true;
 }
@@ -318,8 +460,44 @@ std::optional<Fmg::FmgEntry> Fmg::GetSameEntry(const int& id,
 
     return result;
 }
-
 */
+
+void Esd::AddCommandCall(std::vector<CommandCall>& callsVector, const uint64_t& dataStart) {
+    CommandCall call;
+    GetInt32(call.commandBank); // assert 1/3/5/7?
+    debugLog("call.commandBank" + std::to_string(call.commandBank));
+
+    GetInt32(call.commandId);
+    debugLog("call.commandId" + std::to_string(call.commandId));
+
+    int64_t argsOffset;
+    GetInt64(argsOffset);
+    debugLog("argsOffset" + std::to_string(argsOffset));
+
+    int64_t argsCount;
+    GetInt64(argsCount);
+    debugLog("argsCount" + std::to_string(argsCount));
+
+    StepIn(dataStart + argsOffset, *istream);
+    {
+        for (int i = 0; i < argsCount; i++) {
+            int64_t argOffset;
+            GetInt64(argOffset);
+            debugLog("argOffset" + std::to_string(argOffset));
+
+            int64_t argSize;
+            GetInt64(argSize);
+            debugLog("argSize" + std::to_string(argSize));
+
+            StepIn(dataStart + argOffset, *istream);
+            GetBytes(call.arguments, argSize);
+            StepOut(*istream);
+        }
+    }
+    StepOut(*istream);
+
+    callsVector.push_back(call);
+}
 
 Esd::~Esd() {}
 
--- modules/BBFormats/Esd.h
+++ modules/BBFormats/Esd.h
@@ -10,6 +10,29 @@ namespace FileHelper {
 class Esd : public BBFormat {
     Q_OBJECT
 
+    struct CommandCall {
+        int commandBank; // Should be 1, 5, 6, or 7.
+        int commandId;
+        std::vector<char> arguments;
+    };
+
+    struct Condition {
+        int64_t stateOffset;
+        std::optional<int64_t> targetState = std::nullopt;
+        std::vector<char> evaluator;
+        std::vector<CommandCall> passCommands;
+        std::vector<Condition> subconditions;
+        std::vector<int64_t> conditionOffsets;
+    };
+
+    struct State { /// <summary>
+        int64_t id;
+        std::vector<CommandCall> entryCommands;
+        std::vector<CommandCall> exitCommands;
+        std::vector<CommandCall> whileCommands;
+        std::vector<int64_t> conditionOffsets;
+    };
+
 public:
     explicit Esd(std::vector<char> data, ModMerger* parent);
     ~Esd() override;
@@ -22,10 +45,13 @@ private:
     // std::optional<FmgEntry> GetSameEntry(const int& id, const std::vector<FmgEntry>&
     // otherEntries);
 
+    void AddCommandCall(std::vector<CommandCall>& callsVector, const uint64_t& dataStart);
+
     bool longFormat;
     int gameNumber = 2;
     int Unk70, Unk74, Unk78, Unk7C;
     std::string name = "";
+    std::vector<State> states;
 };
 
 } // namespace FileHelper
